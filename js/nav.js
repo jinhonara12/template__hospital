19a8d6f71288cc12723a7edf8b77ef491fa046de
홈피온 패치워크 <admin@hompion.com>
2023-06-30 15:22:32 +0900

    메인 반응형 완료

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,9 @@
 window.addEventListener('load', function () {
     let header = document.querySelector('header');
     let mainLinks = document.querySelectorAll('nav .menu__box .main__link');
-    let mobileBox = document.querySelector('#mobile_box')
-    let mobileBtn = document.querySelector('#mobile__btn')
+    let mobileBox = document.querySelector('#mobile_box');
+    let mobileBtn = document.querySelector('#mobile__btn');
+    let mobileMenus = document.querySelectorAll('.m__menu');
 
     function hoverMainLink() {
         if (!header.classList.contains('active')) {
@@ -27,6 +28,16 @@ window.addEventListener('load', function () {
     header.addEventListener('mouseleave', outHeader)
     mobileBtn.addEventListener('click', clickMobileMenu)
 
+    function clickMobileLink() {
+        for (el of mobileMenus) {
+            el.classList.remove('active')
+        }
+        this.classList.add('active')
+    }
+    
+    for (el of mobileMenus) {
+        el.addEventListener('click', clickMobileLink)
+    }
 
 
 })
