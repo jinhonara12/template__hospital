bb5c45784e32e32c677b6fb35c84a2efcf0eb50f
jinho <jinhonara12@naver.com>
2023-07-04 17:07:39 +0900

    회사소개 내 웹 연혁 추가 완료

--- js/nav.js
+++ js/nav.js
@@ -1,43 +1,44 @@
 window.addEventListener('load', function () {
-    let header = document.querySelector('header');
-    let mainLinks = document.querySelectorAll('nav .menu__box .main__link');
-    let mobileBox = document.querySelector('#mobile_box');
-    let mobileBtn = document.querySelector('#mobile__btn');
-    let mobileMenus = document.querySelectorAll('.m__menu');
+    this.setTimeout(() => {
+        let header = document.querySelector('header');
+        let mainLinks = document.querySelectorAll('nav .menu__box .main__link');
+        let mobileBox = document.querySelector('#mobile_box');
+        let mobileBtn = document.querySelector('#mobile__btn');
+        let mobileMenus = document.querySelectorAll('.m__menu');
 
-    function hoverMainLink() {
-        if (!header.classList.contains('active')) {
-            header.classList.add('active')
+        function hoverMainLink() {
+            if (!header.classList.contains('active')) {
+                header.classList.add('active')
+            }
         }
-    }
-    
-    function outHeader() {
-        if (header.classList.contains('active')) {
-            header.classList.remove('active')
+
+        function outHeader() {
+            if (header.classList.contains('active')) {
+                header.classList.remove('active')
+            }
         }
-    }
 
-    function clickMobileMenu() {
-        mobileBox.classList.toggle('active')
-    }
+        function clickMobileMenu() {
+            mobileBox.classList.toggle('active')
+        }
 
 
-    for (el of mainLinks) {
-        el.addEventListener('mouseenter', hoverMainLink)
-    }
-    header.addEventListener('mouseleave', outHeader)
-    mobileBtn.addEventListener('click', clickMobileMenu)
+        for (el of mainLinks) {
+            el.addEventListener('mouseenter', hoverMainLink)
+        }
+        header.addEventListener('mouseleave', outHeader)
+        mobileBtn.addEventListener('click', clickMobileMenu)
 
-    function clickMobileLink() {
+        function clickMobileLink() {
+            for (el of mobileMenus) {
+                el.classList.remove('active')
+            }
+            this.classList.add('active')
+        }
+
         for (el of mobileMenus) {
-            el.classList.remove('active')
+            el.addEventListener('click', clickMobileLink)
         }
-        this.classList.add('active')
-    }
-    
-    for (el of mobileMenus) {
-        el.addEventListener('click', clickMobileLink)
-    }
-
-
+    },
+        400)
 })
